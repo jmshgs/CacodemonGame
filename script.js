635c3b66563d162b93fc3eb78dbc38cf7183beab
jam3shug <jameshug678@gmail.com>
2022-04-27 20:08:58 -0700

    First Working Game :)

---
+++ script.js
@@ -0,0 +1,35 @@
+const cacodemon = document.getElementById("cacodemon");
+const rocket = document.getElementById("rocket");
+const score = document.getElementById("score");
+
+function jump() {
+  cacodemon.classList.add("jumpAnimation");
+  setTimeout(() =>
+    cacodemon.classList.remove("jumpAnimation"), 625);
+}
+
+document.addEventListener('keypress', (event) => {
+  if (!cacodemon.classList.contains('jumpAnimation') && event.key == " ") {
+    jump();
+  }
+})
+
+setInterval(() => {
+  const cacodemonTop = parseInt(window.getComputedStyle(cacodemon)
+    .getPropertyValue('top'));
+  const rocketLeft = parseInt(window.getComputedStyle(rocket)
+    .getPropertyValue('left'));
+  score.innerText++;
+
+  if (rocketLeft < 0) {
+    rocket.style.display = 'none';
+  } else {
+    rocket.style.display = ''
+  }
+
+  if (rocketLeft < 100 && cacodemonTop == 25) {
+    alert("You got a score of: " + score.innerText +
+      "\n\nPlay again?");
+    location.reload();
+  }
+}, 50);
