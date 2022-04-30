bfe3613413e4d0de3b4520b51e041da8c269fc9c
jam3shug <jameshug678@gmail.com>
2022-04-30 13:39:35 -0700

    indentation changes

--- script.js
+++ script.js
@@ -2,23 +2,24 @@ const cacodemon = document.getElementById("cacodemon");
 const rocket = document.getElementById("rocket");
 const score = document.getElementById("score");
 
-function jump() {
+function jump() 
+{
   cacodemon.classList.add("jumpAnimation");
   setTimeout(() =>
     cacodemon.classList.remove("jumpAnimation"), 625);
 }
 
-document.addEventListener('keypress', (event) => {
+document.addEventListener('keypress', (event) => 
+{
   if (!cacodemon.classList.contains('jumpAnimation') && event.key == " ") {
     jump();
   }
 })
 
-setInterval(() => {
-  const cacodemonTop = parseInt(window.getComputedStyle(cacodemon)
-    .getPropertyValue('top'));
-  const rocketLeft = parseInt(window.getComputedStyle(rocket)
-    .getPropertyValue('left'));
+setInterval(() => 
+{
+  const cacodemonTop = parseInt(window.getComputedStyle(cacodemon).getPropertyValue('top'));
+  const rocketLeft = parseInt(window.getComputedStyle(rocket).getPropertyValue('left'));
   score.innerText++;
 
   if (rocketLeft < 100 && cacodemonTop == 25) {
